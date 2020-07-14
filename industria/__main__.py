1654744f83216149463d82535602ed6b42b09a4d
Kevin Lee <k323lee@uwaterloo.ca>
2020-07-14 01:32:16 -0400

    implemented click cli prompt into entry point

--- industria/__main__.py
+++ industria/__main__.py
@@ -1,4 +1,38 @@
 import uvicorn
+import click
+import sys
 
-if __name__ == "__main__":
-    uvicorn.run("industria.app:app", host="127.0.0.1", port=5000, log_level="info")
+@click.command()
+@click.option('--host', '-H', type=str, default='127.0.0.1')
+@click.option('--port', '-P', type=int, default=5000)
+def cli(host, port):
+
+    uvicorn.run("industria.app:app", log_level="info", host=host, port=port)
+
+    # I am apparently not smart enough to make a server and window application run at the same time. I will give up on this since this is a non-essential feature.
+    # if appmode:
+    #     def pcef(host, port):
+    #         try:
+    #             from cefpython3 import cefpython as cef
+
+    #             sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+    #             cef.Initialize()
+    #             cef.CreateBrowserSync(url="http://{host}:{prot}",
+    #                                 window_title="Industria")
+    #             cef.MessageLoop()
+    #             cef.Shutdown()
+    #         except ImportError:
+    #             click.echo(click.style('ERROR: You do not have `cefpython3` installed, and therefore cannot run in app mode. `cefpython3` is an optional dependency for this package that is necessary to run this package in app mode. To install, run `python3 -m pip install cefpython3==66.0`.', fg='red'))
+
+    #     p = Process(target=pcef, args=(host, port), daemon=True)
+    #     p.start()
+
+    #     start_server(host, port)
+
+    #     p.join()
+    
+    # else:
+        # start_server(host, port)
+
+if __name__ == '__main__':
+    cli()
